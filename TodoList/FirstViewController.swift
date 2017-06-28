1ca026bd7b79fd9bda5dc32bf084471a3877bdab
Guilherme Bahia <gbvbahia01@gmail.com>
2017-06-28 20:31:01 -0300

    Todo Doing

--- TodoList/FirstViewController.swift
+++ TodoList/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
    override func viewDidLoad() {
       super.viewDidLoad()
@@ -20,6 +20,16 @@ class FirstViewController: UIViewController {
       // Dispose of any resources that can be recreated.
    }
 
+   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+      let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+      cell.textLabel?.text = "Text"
+      return cell
+   }
+   
+   public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 1;
+   }
 
 }
 
